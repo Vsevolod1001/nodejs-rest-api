8e8daa0f508749a257d6377ede1bb1f79670a45f
Vsevolod1001 <sevant.101@gmail.com>
2023-02-20 11:32:10 +0200

    write

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,6 @@
 const contactsServices = require("../services/contacts.service");
-const { createError } = require("../helpers/createError");
+// const { createError } = require("../helpers/createError");
+// const { NetFound } = require("http-errors");
 
 const getAll = async (req, res, next) => {
   try {
@@ -13,10 +14,12 @@ const getById = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await contactsServices.getById(id);
-    if (!result) {
-      throw createError(404, "Not Found");
+    if (result) {
+      // throw new NetFound("Not Found");
+      return res.json(result);
+    } else {
+      return res.status(404).json({ message: "not found" });
     }
-    res.json(result);
   } catch (error) {
     next(error);
   }
