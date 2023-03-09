4a41d25962cb5ecd633f39383767d841e365b314
Vsevolod1001 <sevant.101@gmail.com>
2023-03-09 22:30:59 +0200

    done

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -1,19 +1,28 @@
 const path = require("path");
-const avatarsDir = path.join(__dirname, "../pablic/avatars");
+const avatarsDir = path.join(__dirname, "../public/avatars");
 const fs = require("fs/promises");
 const { User } = require("../models/user");
+const Jimp = require("jimp");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
-  res.json({
-    avatarURL,
-  });
+  try {
+    await Jimp.read(tempUpload).then((item) => {
+      return item.autocrop().cover(250, 250).write(tempUpload);
+    });
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({
+      avatarURL,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = updateAvatar;
